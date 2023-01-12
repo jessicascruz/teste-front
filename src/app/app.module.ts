66060b10b54ef10e4bc591364eb614b69b5430fa
Jéssica Cruz <jessica.santos648@gmail.com>
2023-01-12 00:29:24 -0300

    Criação de cabeçalho responsivo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
+import { CabecalhoComponent } from './cabecalho/cabecalho.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuLateralComponent
+    MenuLateralComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
